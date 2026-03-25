ce7ab91a2d38f7ccde4e0a4dca215e17097d4e3a
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-25 14:34:13 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Hola!!!!!
         
     }
 
