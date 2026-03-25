8f043018702fc24f03c7cc09391769a44b9f198d
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-25 17:02:24 -0500

    MultiplayerLocal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,59 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum PlayerController
+{
+    Player1,
+    Player2
+}
+
 
 public class Player : MonoBehaviour
 {
-    public Player TARGET;
+    [SerializeField] private Health health = new();
 
+    public Player TARGET;
+    public PlayerController ControllerMap;
 
     [SerializeField] string PlayerName;
-    [SerializeField] private Health health = new();
+   
     [SerializeField]private Weapon weapon;
 
+    public InputSystem_Actions inputs;
+
+    public Vector2 moveInput;
+    public int speed;
+
+    private void Awake()
+    {
+        inputs = new();
+    }
+    private void OnEnable()
+    {
+        inputs.Enable();
+        switch (ControllerMap)
+        {
+            case PlayerController.Player1:
+                {
+                    inputs.Player1.Move.performed += OnMoveAction;
+                    inputs.Player1.Move.canceled += OnMoveAction;
+                }
+                break;
+            case PlayerController.Player2:
+                {
+                    inputs.Player2.Move.performed += OnMoveAction;
+                    inputs.Player2.Move.canceled += OnMoveAction;
+                }
+                break;
+        }
+
+    }
+
+    private void OnMoveAction(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
 
     void Start()
     {
@@ -17,6 +62,8 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        transform.position += (Vector3)moveInput * speed * Time.deltaTime;
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Shoot(TARGET);
